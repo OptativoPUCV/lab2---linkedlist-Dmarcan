f83afc9059f594a31a7539f96b5321882edfd458
Replit user <>
2023-03-24 11:32:09 +0000

    new try

--- list.c
+++ list.c
@@ -30,6 +30,7 @@ Node * createNode(void * data) {
 
 List * createList() {
   List * lista =(List *)malloc(sizeof(List));
+  if (lista==NULL)EXIT_FAILURE;
   lista->head=NULL;
   lista->tail=NULL;
   lista->current=NULL;
@@ -141,7 +142,6 @@ void * popCurrent(List * list) {
       list->current=list->current->next;
       free(nodoelimin);
       return data;
-      
     }
   }
 }
